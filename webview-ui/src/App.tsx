524b3f067f9349f7ee60122788111a9ee35867d2
Ivan Mercedes <ivanmercede@gmail.com>
2025-05-19 02:45:34 -0400

    feat: integrate Tailwind CSS for improved styling and layout
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated App.css to include Tailwind CSS and defined a custom dark variant.
    - Refactored App.tsx to enhance the table layout with Tailwind CSS classes for better responsiveness and styling.
    - Adjusted the Vite configuration to include the Tailwind CSS plugin.

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -37,32 +37,54 @@ function App() {
     };
   }, []);
   return (
-    <div>
+    <div className="py-5">
       {data ? (
-        <>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Method</th>
-                <th>URI</th>
-                <th>Action</th>
-                <th>Middleware</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((item: Route) => (
-                <tr key={item.action}>
-                  <td>{item.method}</td>
-                  <td>{item.uri}</td>
-                  <td>{item.name}</td>
-                  <td>{item.action}</td>
-                  <td>{item.middleware}</td>
+        <div>
+          <div className="mb-3">
+            <h2 className="text-2xl">Laravel Route List</h2>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="min-w-full w-full  divide-y-2 divide-gray-200 dark:divide-gray-600">
+              <thead className="ltr:text-left rtl:text-right">
+                <tr className="font-medium text-gray-900 dark:text-gray-200">
+                  <th className="px-3 py-2 whitespace-nowrap">Method</th>
+                  <th className="px-3 py-2 whitespace-nowrap">Name</th>
+                  <th className="px-3 py-2 whitespace-nowrap">URI</th>
+                  <th className="px-3 py-2 whitespace-nowrap">Action</th>
+                  <th className="px-3 py-2 whitespace-nowrap">Middleware</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
+              </thead>
+
+              <tbody className="divide-y divide-gray-200 dark:divide-gray-600">
+                {data?.map((item: Route) => (
+                  <tr
+                    key={item.action}
+                    className="text-gray-900 dark:text-gray-200 first:font-medium"
+                  >
+                    <td className="px-3 py-2 whitespace-nowrap">
+                      {item.method}
+                    </td>
+                    <td className="px-3 py-2 whitespace-nowrap">
+                      {item.name ?? "N.A"}
+                    </td>
+                    <td className="px-3 py-2 whitespace-nowrap">{item.uri}</td>
+
+                    <td className="px-3 py-2 whitespace-nowrap">
+                      {item.action}
+                    </td>
+                    <td className="px-3 py-2 whitespace-nowrap flex flex-wrap">
+                      {item.middleware.map((item: string) => (
+                        <span className="mx-1 border p-1 border-gray-200 dark:border-gray-600">
+                          {item}
+                        </span>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       ) : null}
     </div>
   );
