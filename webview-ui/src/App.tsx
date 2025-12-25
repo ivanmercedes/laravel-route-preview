178a2398b2453754e1c7ac5d1807d5f1c2be95fd
Ivan Mercedes <ivanmercede@gmail.com>
2025-12-25 12:11:52 -0400

    refactor: remove clear filters button from UI

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -313,28 +313,6 @@ function App() {
                     selectedMethods={selectedMethods}
                     onFilterChange={handleMethodFilterChange}
                   />
-                  {hasActiveFilters && (
-                    <button
-                      onClick={handleClearFilters}
-                      className="clear-filters-btn-compact"
-                      title="Clear all filters"
-                    >
-                      <svg
-                        className="w-3 h-3"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                      Clear
-                    </button>
-                  )}
                 </div>
               </div>
             </div>
